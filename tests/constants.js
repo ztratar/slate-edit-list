b6fab353a738852f29ce895e6a441366ed6f80af
Patrik Šlárka <patrik.slarka@productboard.com>
2020-02-19 15:32:57 +0100

    test: add test for getItemDepth util function, refactor it according to the new API

---
+++ tests/constants.js
@@ -0,0 +1,65 @@
+export const COMPLICATED_LIST = [
+    {
+        type: 'ul_list',
+        children: [
+            {
+                type: 'list_item',
+                children: [
+                    {
+                        type: 'ul_list',
+                        children: [
+                            {
+                                type: 'list_item',
+                                children: [
+                                    {
+                                        type: 'ul_list',
+                                        children: [
+                                            {
+                                                type: 'list_item',
+                                                children: [
+                                                    {
+                                                        type: 'ul_list',
+                                                        children: [
+                                                            {
+                                                                type:
+                                                                    'list_item',
+                                                                children: [
+                                                                    {
+                                                                        type:
+                                                                            'ul_list',
+                                                                        children: [
+                                                                            {
+                                                                                type:
+                                                                                    'list_item',
+                                                                                children: [
+                                                                                    {
+                                                                                        type:
+                                                                                            'paragraph',
+                                                                                        children: [
+                                                                                            {
+                                                                                                text:
+                                                                                                    'Complicated list item',
+                                                                                            },
+                                                                                        ],
+                                                                                    },
+                                                                                ],
+                                                                            },
+                                                                        ],
+                                                                    },
+                                                                ],
+                                                            },
+                                                        ],
+                                                    },
+                                                ],
+                                            },
+                                        ],
+                                    },
+                                ],
+                            },
+                        ],
+                    },
+                ],
+            },
+        ],
+    },
+];
