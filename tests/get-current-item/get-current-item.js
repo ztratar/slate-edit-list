c5058d78114ad3fc09845e918897219bf603f86a
Tomas Prchal <tomas.prchal@productboard.com>
2020-02-19 12:07:38 +0100

    feat: add prettier config

--- tests/get-current-item/get-current-item.js
+++ tests/get-current-item/get-current-item.js
@@ -1,49 +1,50 @@
-import expect from "expect";
-import {createEditor, Transforms} from "slate";
+import expect from 'expect';
+import { createEditor, Transforms } from 'slate';
 
-import { withReact } from "slate-react";
-import { getCurrentItem } from "../../lib/utils";
-import Options from "../../lib/options";
+import { withReact } from 'slate-react';
+import { getCurrentItem } from '../../lib/utils';
+import Options from '../../lib/options';
 
 const value = [
-  {
-    type: "ul_list",
-    children: [
-      {
-        type: "list_item",
-        key: "previous_item",
+    {
+        type: 'ul_list',
         children: [
-          {
-            type: "paragraph",
-            children: [{ text: "First item" }]
-          }
-        ]
-      },
-      {
-        type: "list_item",
-        key: "current_item",
-        children: [
-          {
-            type: "paragraph",
-            children: [{ key: "cursor", text: "Second item" }]
-          }
-        ]
-      }
-    ]
-  }
+            {
+                type: 'list_item',
+                key: 'previous_item',
+                children: [
+                    {
+                        type: 'paragraph',
+                        children: [{ text: 'First item' }],
+                    },
+                ],
+            },
+            {
+                type: 'list_item',
+                key: 'current_item',
+                children: [
+                    {
+                        type: 'paragraph',
+                        children: [{ key: 'cursor', text: 'Second item' }],
+                    },
+                ],
+            },
+        ],
+    },
 ];
 
-describe("slate-edit-list", () => {
-  it("testik", () => {
-    const editor = withReact(createEditor());
-    editor.children = value;
+describe('getCurrentItem', () => {
+    describe('from selection', () => {
+        it('returns first parent list item', () => {
+            const editor = withReact(createEditor());
+            editor.children = value;
 
-    Transforms.select(editor, [0,1]);
+            Transforms.select(editor, [0, 1]);
 
-    const opt = new Options();
-    const currentItem = getCurrentItem(opt, editor);
+            const opt = new Options();
+            const currentItem = getCurrentItem(opt, editor);
 
-
-    expect(currentItem.key).toBe("current_item");
-  });
+            expect(currentItem.key).toBe('current_item');
+        });
+    });
 });
