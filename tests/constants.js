0791d4025ff7cc39285a006a628d2df6333b4515
Patrik Šlárka <patrik.slarka@productboard.com>
2020-02-20 10:01:23 +0100

    chore: move list trees to constants for reusability

--- tests/constants.js
+++ tests/constants.js
@@ -1,3 +1,66 @@
+export const PARAGRAPH = [
+    {
+        type: 'paragraph',
+        children: [
+            {
+                text: 'Just a text',
+            },
+        ],
+    },
+];
+
+export const SIMPLE_LIST = [
+    {
+        type: 'ul_list',
+        children: [
+            {
+                type: 'list_item',
+                children: [
+                    {
+                        type: 'paragraph',
+                        children: [
+                            {
+                                text: 'List item',
+                            },
+                        ],
+                    },
+                ],
+            },
+        ],
+    },
+];
+
+export const NESTED_LIST = [
+    {
+        type: 'ul_list',
+        children: [
+            {
+                type: 'list_item',
+                children: [
+                    {
+                        type: 'ul_list',
+                        children: [
+                            {
+                                type: 'list_item',
+                                children: [
+                                    {
+                                        type: 'paragraph',
+                                        children: [
+                                            {
+                                                text: 'Nested list item',
+                                            },
+                                        ],
+                                    },
+                                ],
+                            },
+                        ],
+                    },
+                ],
+            },
+        ],
+    },
+];
+
 export const COMPLICATED_LIST = [
     {
         type: 'ul_list',
